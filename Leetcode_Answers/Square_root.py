3dc5cfb1518abc3b9c51193eec940e2db1101bbf
GannonHiggins <gannonhiggins@gmail.com>
2026-01-29 09:39:12 -0700

    Refactor square_root.py to implement binary search for square root calculation, improving performance. Added test cases for various inputs.

--- Leetcode_Answers/Square_root.py
+++ Leetcode_Answers/Square_root.py
@@ -6,9 +6,28 @@ You must not use any built-in exponent function or operator.
 For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 
 SOLVED
-runtime 0ms
-memory 12.55 MB
+runtime 3ms
+memory 12.52 MB
 '''
 
 def square_root(x):
-    return int(pow(x, 0.5))
+    if x < 2:
+        return x
+    lo, hi = 1, x
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if mid * mid <= x:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return hi
+
+print(square_root(4))
+print(square_root(8))
+print(square_root(16))
+print(square_root(25))
+print(square_root(36))
+print(square_root(49))
+print(square_root(64))
+print(square_root(81))
+print(square_root(100))
