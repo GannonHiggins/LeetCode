0c0a62962f4319e28be2316917eef1879b5cceb7
GannonHiggins <gannonhiggins@gmail.com>
2026-02-19 09:34:53 -0700

    Add remove_deplicates.py to implement a function for removing duplicates from a sorted list, including test cases for various input scenarios.

---
+++ Leetcode_Answers/remove_deplicates.py
@@ -0,0 +1,12 @@
+def remove_deplicates(nums):
+    k = 0
+    for i in range(len(nums)):
+        if i >= 0 and nums[i] != nums[i-1]:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+print(remove_deplicates([1,1,2]))
+print(remove_deplicates([0,0,1,1,1,2,2,3,3,4]))
+print(remove_deplicates([]))
+print(remove_deplicates([1]))
