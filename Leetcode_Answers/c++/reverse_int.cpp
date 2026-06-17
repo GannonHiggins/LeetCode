77b4d3693c78c238e645e5ef9deee66d8cdaaba6
GannonHiggins <gannonhiggins@gmail.com>
2026-06-17 11:30:26 -0600

    Add detailed comments to reverse_int.cpp, including the problem statement and performance metrics. This enhances clarity and provides context for the implementation of the integer reversal function.

--- Leetcode_Answers/c++/reverse_int.cpp
+++ Leetcode_Answers/c++/reverse_int.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+SOLVED:
+runtime 5ms
+memory 8.59 MB
+*/
+
 int reverse_int(int x){
     long long rev = 0;
     while (x != 0) {
