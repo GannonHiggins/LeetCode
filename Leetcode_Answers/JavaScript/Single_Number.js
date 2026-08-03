36f46d30099479e932b7b352a214dbd8747314c6
GannonHiggins <gannonhiggins@gmail.com>
2026-08-03 08:31:53 -0600

    Add RomanToInt.js to implement a function that converts a Roman numeral to an integer. The solution includes a mapping of Roman symbols to values and handles subtraction cases, with test cases demonstrating correct functionality and performance metrics.

---
+++ Leetcode_Answers/JavaScript/Single_Number.js
@@ -0,0 +1,20 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+SOLVED:
+0ms Runtime beats 100% of users
+54.22 MB Memory Usage beats 94.49% of users
+*/
+
+var singleNumber = function (nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+  return result;
+};
+
+console.log(singleNumber([2, 2, 1])); // 1
+console.log(singleNumber([4, 1, 2, 1, 2])); // 4
+console.log(singleNumber([1])); // 1
